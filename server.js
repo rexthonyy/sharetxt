0eef2ee623c158a36b93fbea9b956093c059850c
rexthonyy <rexthonyy@gmail.com>
2021-02-08 11:01:32 +0100

    Removed the button and added input listener

--- server.js
+++ server.js
@@ -1,41 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
 
 const app = express();
 const server = http.Server(app);
 
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || process.env.LOCAL_PORT;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-const wss = new WebSocket.Server({ server });
-
-let clients = [];
-
-wss.on('connection', (ws) => {
-
-	clients.push(ws);
-	
-	console.log(clients.length + " Clients connected");
-	
-	ws.on('message', (msg) => {
-		clients.forEach(client => {
-			if(client != ws && client.readyState === WebSocket.OPEN){//send a message to all other clients but yourself
-				client.send(msg);
-			}
-		});
-	});
-
-	ws.on('close', () => {
-		for(let i = 0; i < clients.length; i++){
-			if(clients[i] == ws){
-				clients.splice(i);
-				break;
-			}
-		}
-		console.log("Client left");
-		console.log(clients.length + " clients remaining");
-	});
-});
