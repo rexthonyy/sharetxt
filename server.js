95a24a0e74e52762960ca2b9d29cc9c1350ba7d6
rexthonyy <rexthonyy@gmail.com>
2021-02-08 11:28:25 +0100

    Re-implemented the server.js

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
-require('dotenv').config();
 const express = require('express');
 const http = require('http');
+const { Server } = require('ws');
 
 const app = express();
 const server = http.Server(app);
 
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || process.env.LOCAL_PORT;
+const LOCAL_PORT = 3000;
+const PORT = process.env.PORT || LOCAL_PORT;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+const wss = new Server({ server });
+
+let clients = [];
+
+wss.on('connection', (ws) => {
+
+  clients.push(ws);
+  console.log(clients.length + " clients connected");
+
+  ws.on('message', (msg) => {
+	  clients.forEach(client => {
+		if(client != ws && client.readyState === WebSocket.OPEN){
+			client.send(msg);
+		}
+	  });
+  });
+
+  ws.on('close', () => {
+	  for(let i = 0; i < clients.length; i++){
+		  if(clients[i] == ws){
+			  clients.splice(i);
+		  }
+	  }
+	  
+	  console.log(clients.length + " clients remaining");
+  });
+});
