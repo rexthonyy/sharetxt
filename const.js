13d39be0c9cd889a9e4f088bb544e195087ef2e6
Rex Anthony <66477467+rexthonyy@users.noreply.github.com>
2021-09-21 17:11:45 +0100

    Updated credentials

--- const.js
+++ const.js
@@ -1,22 +1,22 @@
 let appName = "ShareTXT";
-let appEmail = "sharetxtapp@gmail.com";
-let appEmailPass= "sharetxt123app";
+let appEmail = "";
+let appEmailPass= "";
 let sendEmailUrl = "https://email-dispatcher-api.herokuapp.com/api/v1";
 
-let googleClientId = "1009354150419-l0a4m06hh54kkspnek8drle703o9mfa6.apps.googleusercontent.com";
-let googleClientSecret = "MRwKPkokVcQctA3pBa7EN1w5";
+let googleClientId = "";
+let googleClientSecret = "";
 //let googleCallbackUrl = "http://localhost:3000/auth/google/callback";
 let googleCallbackUrl = "https://rexshare.herokuapp.com/auth/google/callback";
 
 
-let facebookAppID = "184211980360651";
-let facebookAppSecret = "5586c3d3fad28813ea82cef4db7c13e6";
+let facebookAppID = "";
+let facebookAppSecret = "";
 //let facebookCallbackUrl = "http://localhost:3000/auth/facebook/callback";
 let facebookCallbackUrl = "https://rexshare.herokuapp.com/auth/facebook/callback";
 
 
-let twitterConsumerKey = "lddigO2q5m5l2LDQwSBQWUrEe";
-let twitterConsumerSecret = "0nLSF50tDYArCu8YSlcxhpkh7lSNhosue09b4MNNVZz7viHtpe";
+let twitterConsumerKey = "";
+let twitterConsumerSecret = "";
 //let twitterCallbackUrl = "http://localhost:3000/auth/twitter/callback";
 let twitterCallbackUrl = "https://rexshare.herokuapp.com/auth/twitter/callback";
 
